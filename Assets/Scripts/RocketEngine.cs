44cffe237799a90398ce0d9e023345265b155a6a
Peter Bush <poytebush@gmail.com>
2020-07-20 15:17:34 +0100

    Wicket WeRocket

--- Assets/Scripts/RocketEngine.cs
+++ Assets/Scripts/RocketEngine.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PhysicsObject))]
-public class ForceSource : MonoBehaviour
+public class RocketEngine : MonoBehaviour
 {
     public Vector3 force;
 
