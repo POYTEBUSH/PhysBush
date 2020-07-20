fad4567e2d85a1426a91792898bcd5b145d3c756
Peter Bush <poytebush@gmail.com>
2020-07-20 11:02:00 +0100

    Keep
    !

---
+++ Assets/PhysicsEngine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour
+{
+    public Vector3 v; //average velocity this fixed update.
+
+    void FixedUpdate()
+    {
+        Vector3 deltaS = v *
+    }
+}
