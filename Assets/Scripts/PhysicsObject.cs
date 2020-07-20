320a99b316256115194a7748a64d5f30186a1dc2
Peter Bush <poytebush@gmail.com>
2020-07-20 16:39:15 +0100

    Gravity

--- Assets/Scripts/PhysicsObject.cs
+++ Assets/Scripts/PhysicsObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PhysicsObject : MonoBehaviour
@@ -11,21 +12,51 @@ public class PhysicsObject : MonoBehaviour
 
     public ForceContainer forceContainer = new ForceContainer(); // All the forces currently applied to this object.
 
+	private PhysicsObject[] physicsObjects;
+
 	void Start()
     {
 		SetupThrustTrailRenderer();
 		mass += GetComponent<RocketEngine>().fuelMass;
+
+		physicsObjects = FindObjectsOfType<PhysicsObject>();
     }
 
     void FixedUpdate()
     {
-        UpdateVelocity();		
+		CalculateGravity();
+        UpdateVelocity();
+		
         UpdateThrustTrailRenderer();
 
         Vector3 deltaS = velocity * Time.deltaTime;
         transform.position += deltaS;
 
         forceContainer.ClearForces();
+    }
+
+    void CalculateGravity()
+    {
+        foreach (PhysicsObject physicsObjectA in physicsObjects)
+        {
+			foreach (PhysicsObject physicsObjectB in physicsObjects)
+			{
+				if (physicsObjectA != physicsObjectB)
+                {
+					Debug.Log("Calulating the force applied to " + physicsObjectA.name + " relating to the gravity of " + physicsObjectB.name);
+
+					const float gravitationalConstant = 6.673e-11f; // [m^3 s^-2 kg^-1]
+
+					Vector3 offset = physicsObjectA.transform.position - physicsObjectB.transform.position;
+					float rSquared = (1.0f / Mathf.Pow(offset.magnitude, 2.0f));
+					float gravityMagnitude = (gravitationalConstant * ((physicsObjectA.mass * physicsObjectB.mass) / rSquared));
+
+					Vector3 graviationalForce = gravityMagnitude * offset.normalized;
+
+					physicsObjectA.forceContainer.AddForce(-graviationalForce);
+                }
+			}
+        }
     }
 
     private void UpdateVelocity()
