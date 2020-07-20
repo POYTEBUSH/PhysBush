f19b8e131c6b310cf8090c4c4dca0af98a7f1fc7
Peter Bush <poytebush@gmail.com>
2020-07-20 13:27:06 +0100

    Accumulation of forces

--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -1,15 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class PhysicsEngine : MonoBehaviour
 {
-    public Vector3 velocity; //average velocity this fixed update.
+    public Vector3 velocity; // The average velocity this fixed update.
+
+    public Vector3 netForce; // The net force applied to this physics object.
+    public List<Vector3> forces; // All the forces currently applied to this object.
 
     void FixedUpdate()
     {
-        Vector3 deltaS = velocity * Time.deltaTime;
+        CalculateNetForce();
 
-        transform.position += deltaS;
+        if (netForce == Vector3.zero)
+        {            
+            Vector3 deltaS = velocity * Time.deltaTime;
+            transform.position += deltaS;
+        }
+        else
+        {
+            Debug.LogError("Unbalanced force detected. FUCK");
+        }
+    }
+
+    private void CalculateNetForce()
+    {
+        Vector3 totalForce = forces[0];
+        for (int i = 1; i < forces.Count; i++)
+        {
+            totalForce += forces[i];
+        }
+        netForce = totalForce / forces.Count;
     }
 }
